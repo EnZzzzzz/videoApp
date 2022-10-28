7a7ad300f92af1e21cda854266ff464914be2f9f
enze <957078942@qq.com>
2022-10-29 00:05:18 +0800

    重构一下cmakelist

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <GLFW/glfw3.h>
 
 #include "export/OffscreenCtx.h"
-//#include "exclude/Window.h"
+#include "export/Window.h"
 #include "effectTasks/TaskTriangle.h"
 
 int main()
 {
-    OffscreenCtx ctx = OffscreenCtx(630, 360, "test");
-//    Window wid = Window(630, 360, "test");
+//    OffscreenCtx ctx = OffscreenCtx(630, 360, "test");
+    auto ctx = Window(630, 360, "test");
 
     Task* task;
     TaskTriangle tri = TaskTriangle();
